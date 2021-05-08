16248d4845fffffbb42d670f41a912a92ef72236
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-08 17:38:11 -0400

    Add auth service, login & register forms

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
+	Link
 } from 'react-router-dom';
 
 import { getUsers } from './services/db/userService';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
+
+import { AuthType } from './models/User';
+
 import Home from './pages/Home/Home';
+import UserForm from './pages/UserForm/UserForm';
 import Book from './components/Book/Book';
 
 const App = (): JSX.Element => {
@@ -28,6 +33,16 @@ const App = (): JSX.Element => {
 					<div className="header py-2 px-3 text-navbar">
 						<span>Open Book</span>
 						<div>
+							<button>
+								<Link to="/signup">
+									Sign Up
+								</Link>
+							</button>
+							<button>
+								<Link to="/signin">
+									Sign In
+								</Link>
+							</button>
 							<span className="user-info">{user.name}</span>
 							<span className="user-info">{user.points}</span>
 						</div>
@@ -41,13 +56,19 @@ const App = (): JSX.Element => {
 								<Home />
 							</Route>
 							<Route path="/book/:id">
-								<Book/>
+								<Book />
+							</Route>
+							<Route path="/signup">
+								<UserForm authType={AuthType.SignUp} />
+							</Route>
+							<Route path="/signin">
+								<UserForm authType={AuthType.SignIn} />
 							</Route>
 						</Switch>
 					</div>
 				</div >
 			</div>
-		</Router >
+		</Router>
 	);
 };
 
