f24b66acc04e36b8d830b59ebf286fa0e67a0553
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-08 18:01:23 -0400

    Extend book service

--- src/services/db/bookService.ts
+++ src/services/db/bookService.ts
@@ -2,12 +2,21 @@ import Book from '../../models/Book';
 import { db } from '../firebaseConfig';
 
 class BookService {
-	// returns undefined if not found
-	static getBook = async (bookId: string): Promise<Book> => {
-		const booksRef = await db.collection('books').doc(bookId).get();
-		const result = booksRef.data() as Book;
+	static getBooks = async (): Promise<Book[]> => {
+		const booksRef = await db.collection('books').get();
+		const result = booksRef.docs.map(book => book.data() as Book);
 		return result;
 	};
+
+	static getBook = async (bookId: string): Promise<Book> => {
+		const bookRef = await db.collection('books').doc(bookId).get();
+		const result = bookRef.data() as Book;
+		return result;
+	};
+
+	static addBook = async (book: Book) => {
+		await db.collection('books').add(book);
+	}
 }
 
 export default BookService;
