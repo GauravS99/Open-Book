15c569bbc5c7176d9ccbbfe3d0bf04b83e3ebebd
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-09 00:27:53 -0400

    Add book view tab header styles

--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -1,9 +1,10 @@
+import './Book.css';
 import React, { useState, useEffect } from 'react';
 import Book from '../../models/Book';
 import BookService from '../../services/db/bookService';
 import BookActionHeader, { BookHeaderAction } from './BookActionHeader';
 import BookDocumentView from './BookDocumentView';
-import {  useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import BookEditList from './BookEditList';
 
 interface PropTypes {
@@ -36,37 +37,37 @@ const BookComponent = (props: PropTypes): JSX.Element => {
 	const book: Book | null = useFetchBook(id);
 	const [tab, setTab] = useState(Tab.story);
 
-	const emphasis = 'fw-bold';
-    
+	const emphasis = 'selected-tab';
+
 	console.log(tab);
 	return (
-		<div className="content bg-colour-2 colour-4 h-100 p-3 rounded">
-			<div>
-				<h6 className={`d-inline-block me-3 clickable ${tab === Tab.story ? emphasis : ''}`} onClick={() => setTab(Tab.story)}>
+		<div className="content colour-4 h-100 p-3 rounded">
+			<div className="tab-headers">
+				<h6 className={`d-inline-block me-3 tab ${tab === Tab.story ? emphasis : ''}`} onClick={() => setTab(Tab.story)}>
 					Story
                 </h6>
-				<h6 className={`me-3 d-inline clickable ${tab === Tab.edits ? emphasis : ''}`} onClick={() => setTab(Tab.edits)}>
+				<h6 className={`me-3 d-inline tab ${tab === Tab.edits ? emphasis : ''}`} onClick={() => setTab(Tab.edits)}>
 					Edits
                 </h6>
-				<h6 className={`me-3 d-inline clickable ${tab === Tab.contributions ? emphasis : ''}`} onClick={() => setTab(Tab.contributions)}>
+				<h6 className={`me-3 d-inline tab ${tab === Tab.contributions ? emphasis : ''}`} onClick={() => setTab(Tab.contributions)}>
 					Contributions
                 </h6>
-            </div>
-            <hr/>
-            <div className="mt-3">
-                <div className="bg-color-5">
-                    {tab === Tab.story &&
-                        <BookDocumentView book={book}/>
-                    }
-                </div>
-                <div>
-                    {tab === Tab.edits &&
-                        <BookEditList book={book}/>
-                    }
-                </div>
-            </div>
-        </div>
-    );
+			</div>
+			<hr />
+			<div className="mt-3">
+				<div className="bg-color-5">
+					{tab === Tab.story &&
+						<BookDocumentView book={book} />
+					}
+				</div>
+				<div>
+					{tab === Tab.edits &&
+						<BookEditList book={book} />
+					}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default BookComponent;
