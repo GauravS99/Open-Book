27aee31b99bf03ce908e60b6c759115d0e5a33bb
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-05-08 22:57:36 -0400

    Add user form styles and update booklist styles

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,3 +1,4 @@
+import './UserForm.css';
 import { FC, useState } from 'react';
 import { AuthType } from '../../models/User';
 import { Link } from 'react-router-dom';
@@ -6,40 +7,39 @@ import React from 'react';
 
 const UserForm: FC<{ authType: AuthType }> = ({ authType }): JSX.Element => {
 
-	const [email, setemail] = useState('');
+	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [username, setUsername] = useState('');
-	
+
 	const handleSignUp = () => signUpUser(email, password, username);
 	const handleSignIn = () => signInUser(email, password);
 
 	return (
-		<div className="p-3 colour-5 form-container">
+		<div className="user-form-container">
 			<div>
 				<div className="text-center">
 					<h4>{authType}</h4>
 				</div>
 				<div className="form-group mt-4">
-					<label>Email</label>
-					<input className="form-control" onChange={event => setemail(event.target.value)} />
-					<label>Password</label>
-					<input className="form-control" onChange={event => setPassword(event.target.value)} />
-					{authType === AuthType.SignUp &&  
+					<input className="form-control" placeholder="Email" onChange={event => setEmail(event.target.value)} />
+					<input className="form-control" placeholder="Password" onChange={event => setPassword(event.target.value)} />
+					{authType === AuthType.SignUp &&
 						<React.Fragment>
-							<label> Username </label>
-							<input className="form-control" onChange={event => setUsername(event.target.value)}/>
-						</React.Fragment>	
+							<input className="form-control" placeholder="Username" onChange={event => setUsername(event.target.value)} />
+						</React.Fragment>
 					}
 				</div>
-				<button className="btn btn-secondary me-2">
-					<Link to="/">Cancel</Link>
-				</button>
-				<button
-					className="btn btn-primary d-inline-block"
-					onClick={authType === AuthType.SignUp ? handleSignUp : handleSignIn}
-				>
-					{authType}
-				</button>
+				<div className="form-btn-container">
+					<button className="form-btn btn btn-secondary">
+						<Link to="/">Cancel</Link>
+					</button>
+					<button
+						className="form-btn btn btn-primary"
+						onClick={authType === AuthType.SignUp ? handleSignUp : handleSignIn}
+					>
+						{authType}
+					</button>
+				</div>
 			</div>
 		</div>
 	);
